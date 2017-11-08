3597dbc2c6325b90ef2d1a1bf6d44233e2c14899
Apps RingByName <Marcelo@Appss-Mac-mini.local>
2017-11-08 17:57:46 -0200

    xx

--- exercicios/exercicio5.rb
+++ exercicios/exercicio5.rb
@@ -42,63 +42,107 @@ class Contato
              puts self.endereco if self.endereco
         end
 
-     end
+end
 
-     puts "Bem vindo ao sistema de contatos"
-     puts "O que você deseja fazer:
-     1 - Cadastrar
-     2 - Alterar
-     3 - Apagar"
-     opcao = STDIN.gets.to_i
-     puts opcao
-
-     lista_contatos = Lista.new
+puts "Bem vindo ao sistema de contatos"
      
-     resposta = 0
+
+lista_contatos = Lista.new
+resposta = 0
+
+#cadastra novos contatos:
+until resposta  == 2 do
+        
+    puts "cadastro"
+    puts "##############"
+        
+                    puts "Informe o nome do contato:"
+                    nome = STDIN.gets
+                    puts "##############"
+        
+                    puts "Informe a email"
+                    email = STDIN.gets
+                    puts "##############"
+        
+                    puts "Informe telefone"
+                    tel = STDIN.gets.to_i     
+                    puts "##############"
+        
+                    puts "Informe endereço"
+                    endereco = STDIN.gets 
+        
+                #monta obejto contato
+                    contato = Contato.new(nome)
+                    contato.email = email
+                    contato.tel = tel
+                    contato.endereco = endereco
+                
+                #adiciona contato a lista
+                    lista_contatos.adiciona(contato)
+                
+                #verifica se o usuário que adicionar mais contatos
+                    puts "####################"
+                    puts "####################"
+                    puts "Você deseja cadastar um novo contato: 
+                    1 - Sim
+                    2 - Nao"
+                    resposta = STDIN.gets.to_i            
+                end
+            #mostra a lista de contatos adicionada
+                puts lista_contatos.mostra
+    puts "O que você deseja fazer:
+    1 - Cadastrar
+    2 - Alterar
+    3 - Apagar"
+                opcao = STDIN.gets.to_i
+                puts opcao
+     
      if opcao == 1
+         #cadastra novos contatos:
+     until resposta  == 2 do
+        
+                    puts "cadastro"
+                    puts "#########◊◊#####"
+        
+                    puts "Informe o nome do contato:"
+                    nome = STDIN.gets
+                    puts "##############"
+        
+                    puts "Informe a email"
+                    email = STDIN.gets
+                    puts "##############"
+        
+                    puts "Informe telefone"
+                    tel = STDIN.gets.to_i     
+                    puts "##############"
+        
+                    puts "Informe endereço"
+                    endereco = STDIN.gets 
+        
+                #monta obejto contato
+                    contato = Contato.new(nome)
+                    contato.email = email
+                    contato.tel = tel
+                    contato.endereco = endereco
+                
+                #adiciona contato a lista
+                    lista_contatos.adiciona(contato)
+                
+                #verifica se o usuário que adicionar mais contatos
+                    puts "####################"
+                    puts "####################"
+                    puts "Você deseja cadastar um novo contato: 
+                    1 - Sim
+                    2 - Nao"
+                    resposta = STDIN.gets.to_i            
+                end
+            #mostra a lista de contatos adicionada
+                puts lista_contatos.mostra
 
         puts "Resposta1: #{resposta}"
                
         #cadastra um novo contato até o usuário selecionar a opção 2
-        until resposta  == 2 do
-
-            puts "cadastro"
-            puts "##############"
-
-            puts "Informe o nome do contato:"
-            nome = STDIN.gets
-            puts "##############"
-
-            puts "Informe a email"
-            email = STDIN.gets
-            puts "##############"
-
-            puts "Informe telefone"
-            tel = STDIN.gets.to_i     
-            puts "##############"
-
-            puts "Informe endereço"
-            endereco = STDIN.gets 
-
-        #monta obejto contato
-            contato = Contato.new(nome)
-            contato.email = email
-            contato.tel = tel
-            contato.endereco = endereco
-        
-        #adiciona contato a lista
-            lista_contatos.adiciona(contato)
-        
-        #verifica se o usuário que adicionar mais contatos
-            puts "####################"
-            puts "####################"
-            puts "Você deseja cadastar um novo contato: 
-            1 - Sim
-            2 - Nao"
-            resposta = STDIN.gets.to_i            
-        end
-    #mostra a lista de contatos adicionada
-        puts lista_contatos.mostra
+       
 
      elsif opcao == 2
     #Edita um contato
@@ -107,11 +151,35 @@ class Contato
     puts "Informe o nome do contato:"
     pesquisa_contato = STDIN.gets
 
-    lista
-        
-    
-        
-     elsif opcao ==3
+    puts "Encontramos, o que você deseja alterar? Informe sua opção:
+    1 - nome
+    2 - email
+    3 - telefone
+    4 - endereco"
+    opcao = STDIN.gets.to_i
+        if opcao == 1 
+             #altera nome
+            "Informe o nome do Contato"
+            altera_nome = STDIN.gets
+
+            lista_contato.each do |item|
+                if item[:nome] == pesquisa_contato
+                item[:nome] = altera_nome
+            end
+        end
+
+            lista_contato.mostra
+        elsif opcao == 2
+            #altera email
+        elsif opcao == 3
+            #altera telefone
+        elsif opcao ==4
+            #altera endereço
+        else
+            "opção invalida."
+        end
+   
+    elsif opcao ==3
         #apaga um contato
         puts "apaga"
      else
